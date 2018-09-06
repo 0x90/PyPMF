73c849d6c08e0bc45f06751bb9c11d211a0f5d9f
0x90 <oleg.kupreev@gmail.com>
2018-09-06 13:33:33 +0300

    setup.py added for "pip install -e ." command support

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='PyPMF',
+    version='1.0.0',
+    py_module='PyPMF',
+    url='https://github.com/feliam/PyPMF',
+    license='GPL',
+    author='feliam',
+    # author_email='feliam@mail.com',
+    description='A small python module to manipulate Windows Internals Process Monitor PMF Filter files'
+)
